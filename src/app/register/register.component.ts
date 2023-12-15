e98e760b9b85ec6752a0aa671f1dc10e4642c2ba
MtFranke <mateusz.franke@proton.me>
2023-12-15 16:41:48 +0100

    simple register form added

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,12 +1,38 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {AbstractControl, FormControl, FormGroup, ReactiveFormsModule, Validators} from "@angular/forms";
+
 
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [],
+  imports: [
+    ReactiveFormsModule
+  ],
   templateUrl: './register.component.html',
   styleUrl: './register.component.css'
 })
-export class RegisterComponent {
+export class RegisterComponent implements OnInit{
 
+
+  public registerForm: FormGroup | any;
+
+
+  ngOnInit() {
+
+  this.registerForm = new FormGroup({
+    'username': new FormControl('', [Validators.required]),
+    'email': new FormControl('', [Validators.required]),
+    'passwords': new FormGroup({
+      'password': new FormControl('', [Validators.required]),
+      'confirm': new FormControl('', [Validators.required])
+    }, {validators: this.validateAreEqual})
+    });
+
+  }
+  public validateAreEqual(c: AbstractControl): { notSame: boolean } | null {
+    return c.value.password === c.value.confirm ? null : { notSame: true };
+  }
+  onSubmit() {
+    console.log(this.registerForm.value);
+  }
 }
