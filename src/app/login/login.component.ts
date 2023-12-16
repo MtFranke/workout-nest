95e1b88b8c4414a38402d85947a47d0d69a3a8ee
MtFranke <mateusz.franke@proton.me>
2023-12-16 17:15:51 +0100

    add login login

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,26 +1,53 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {Router} from "@angular/router";
+import {FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators} from "@angular/forms";
+import {HttpClient, HttpClientModule} from "@angular/common/http";
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [
+    FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule
+  ],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
-export class LoginComponent {
-  constructor(private router: Router) {
+export class LoginComponent implements OnInit{
+
+  public loginForm: FormGroup | any;
+
+  constructor(private router: Router, private http: HttpClient) {
 
   }
 
-
-  onLoginClick() {
-    this.router.navigate(['dashboard']);
-
+  ngOnInit() {
+    this.loginForm = new FormGroup({
+      'username': new FormControl('', [Validators.required]),
+      'password': new FormControl('', [Validators.required])}
+    );
   }
 
   onRegisterClick() {
     this.router.navigate(['register']);
 
+  }
+
+  onSubmit() {
+
+    const url = 'https://workoutnest.azurewebsites.net/auth/login';
+    // const url = 'http://localhost:5213/auth/register';
+    const payload = {
+      'username' : this.loginForm.value['username'],
+      'password' : this.loginForm.value['password']
+    }
+
+    this.http.post(url, payload)
+      .subscribe(
+        (x) => {
+          console.log(x);
+          this.router.navigate(['dashboard']);
+        })
   }
 }
--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,20 +1,27 @@
 import {Component, OnInit} from '@angular/core';
 import {AbstractControl, FormControl, FormGroup, ReactiveFormsModule, Validators} from "@angular/forms";
+import {HttpClient, HttpClientModule} from "@angular/common/http";
+import {NgIf} from "@angular/common";
+import {Router} from "@angular/router";
 
 
 @Component({
   selector: 'app-register',
   standalone: true,
   imports: [
-    ReactiveFormsModule
+    ReactiveFormsModule, HttpClientModule, NgIf
   ],
   templateUrl: './register.component.html',
   styleUrl: './register.component.css'
 })
 export class RegisterComponent implements OnInit{
 
-
+  successfullyRegistered: boolean = false;
+  username: string = '';
   public registerForm: FormGroup | any;
+
+  constructor(private http: HttpClient, private router: Router) {
+  }
 
 
   ngOnInit() {
@@ -33,6 +40,24 @@ export class RegisterComponent implements OnInit{
     return c.value.password === c.value.confirm ? null : { notSame: true };
   }
   onSubmit() {
-    console.log(this.registerForm.value);
+    const url = 'https://workoutnest.azurewebsites.net/auth/register';
+    // const url = 'http://localhost:5213/auth/register';
+    this.username = this.registerForm.value['username'];
+    const payload = {
+      'username' : this.registerForm.value['username'],
+      'email' : this.registerForm.value['email'],
+      'password' : this.registerForm.value['passwords'].password,
+    }
+
+    this.http.post(url, payload)
+      .subscribe(
+        (x) => {
+          console.log(x);
+          this.successfullyRegistered = true;
+        })
+  }
+
+  onGotoDashboard() {
+    this.router.navigate(['dashboard']);
   }
 }
