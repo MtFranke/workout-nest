f8edfaba9dc5027ab15dc0093918fa767a294f74
MtFranke <mateusz.franke@proton.me>
2024-01-28 16:54:01 +0100

    feed component

--- src/app/components/dashboard/feed/feed.component.ts
+++ src/app/components/dashboard/feed/feed.component.ts
@@ -1,12 +1,35 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {environment} from "../../../../environment/environment";
+import {FeedModel} from "./feed.model";
+import {map} from "rxjs";
+import {NgForOf} from "@angular/common";
+import moment from 'moment';
 
 @Component({
   selector: 'app-feed',
   standalone: true,
-  imports: [],
+  imports: [
+    NgForOf
+  ],
   templateUrl: './feed.component.html',
   styleUrl: './feed.component.css'
 })
-export class FeedComponent {
+export class FeedComponent implements OnInit{
 
+  data : FeedModel[] = [];
+
+  constructor(public http: HttpClient) {
+  }
+  ngOnInit() {
+    const url = `${environment.workoutNestApiUrl}/workouts`;
+    this.http
+      .get<FeedModel[]>(url)
+      .subscribe((x)=> {
+        this.data = x;
+        console.log(this.data);
+      });
+  }
+
+  protected readonly moment = moment;
 }
---
+++ src/app/components/dashboard/feed/feed.model.ts
@@ -0,0 +1,6 @@
+export class FeedModel{
+  name: string = '';
+  value: number = 0;
+  daysPassed: number = 0;
+  workoutDate: string = '';
+}
